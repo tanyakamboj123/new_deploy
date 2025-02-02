3ad0b2db8f4e1fd18f1ce43c981b1697b171919d
tanya kamboj <21f3000745@ds.study.iitm.ac.in>
2025-02-02 10:40:07 +0530

    COMMIT7

--- index.py
+++ index.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 import json
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+from fastapi.middleware.cors import CORSMiddleware
 
+app = Flask(__name__)
+app=CORS()
 # Load student data from JSON file
 with open("q-vercel-python.json", "r") as file:
     
